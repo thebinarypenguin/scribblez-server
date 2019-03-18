590c7e1c9f8020f19511503e6b96f7023e97303d
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-18 13:36:37 -0400

    Add configured knex object to app

--- src/server.js
+++ src/server.js
@@ -1,6 +1,14 @@
+const knex   = require('knex');
 const app    = require('./app');
 const config = require('./config');
 const pkg    = require('../package.json');
+
+const db = knex({
+  client: 'pg',
+  connection: config.db_string,
+});
+
+app.set('db', db);
 
 app.listen(config.port, config.host, () => {
   console.log(`${pkg.name}@${pkg.version} running in ${config.node_env} mode at http://${config.host}:${config.port}`);
