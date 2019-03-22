5f01ebc3f54a3c68812539c6d615f25551f2f2f4
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-22 12:39:25 -0400

    Update postgrator config

--- postgrator-config.js
+++ postgrator-config.js
@@ -3,9 +3,9 @@ require('dotenv').config();
 module.exports = {
   'migrationDirectory' : 'migrations',
   'driver'             : 'pg',
-  'host'               : process.env.DB_HOST,
-  'port'               : process.env.DB_PORT,
-  'database'           : process.env.DB_NAME,
-  'username'           : process.env.DB_USER,
-  'password'           : process.env.DB_PASS,
+  'username'           : process.env.DB_MIGRATION_USER,
+  'password'           : process.env.DB_MIGRATION_PASS,
+  'host'               : process.env.DB_MIGRATION_HOST,
+  'port'               : process.env.DB_MIGRATION_PORT,
+  'database'           : process.env.DB_MIGRATION_DATABASE,
 };
