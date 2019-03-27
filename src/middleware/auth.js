ebced2461a0b39f144fe3908d58088915640a46f
Ethan Zimmerman <thebinarypenguin@gmail.com>
2019-03-27 10:06:37 -0400

    Remove unused middleware

--- src/middleware/auth.js
+++ src/middleware/auth.js
@@ -18,71 +18,12 @@ const requireAuth = function (req, res, next) {
     .verifyToken(token, configService.token_secret)
     .then((payload) => {
 
+      // Get info for user associated with token
       return usersService
         .getUserByUsername(req.app.get('db'), payload.username)
         .then((user) => {
-          req.user = user;
-          next();
-        });
-    })
-    .catch((err) => {
-      return res.status(401).json({ error: 'invalid authorization' });
-    });
-};
 
-const checkAuth = function (req, res, next) {
-
-  const authorization = req.get('Authorization');
-
-  // If no Authorization header
-  if (!authorization) {
-    next();
-    return;
-  }
-
-  const token = authorization.slice(7);
-
-  // Validate token
-  tokenService
-    .verifyToken(token, configService.token_secret)
-    .then((payload) => {
-
-      return usersService
-        .getUserById(req.app.get('db'), payload.userId)
-        .then((user) => {
-          req.user = user;
-          next();
-        });
-    })
-    .catch((err) => {
-      return res.status(401).json({ error: 'invalid authorization' });
-    });
-};
-
-const matchingUserAuth = function (req, res, next) {
-
-  const authorization = req.get('Authorization');
-
-  // If no Authorization header
-  if (!authorization) {
-    return res.status(401).json({ error: 'authorization not provided' });
-  }
-
-  const token = authorization.slice(7);
-
-  // Validate token
-  tokenService
-    .verifyToken(token, configService.token_secret)
-    .then((payload) => {
-
-      return usersService
-        .getUserById(req.app.get('db'), payload.userId)
-        .then((user) => {
-
-          if (user.username !== req.params.username) {
-            return res.status(401).json({ error: 'invalid authorization' });
-          }
-
+          // Save user info for use later
           req.user = user;
           next();
         });
@@ -94,6 +35,4 @@ const matchingUserAuth = function (req, res, next) {
 
 module.exports = {
   requireAuth,
-  checkAuth,
-  matchingUserAuth,
 };
